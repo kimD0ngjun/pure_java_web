3d3bcb2f0af48ce68631107642d8c9de51164566
kimD0ngjun <chickentasty0112@gmail.com>
2025-12-03 04:11:38 +0900

    feat: 위상정렬 기반 빈 생성 및 의존성 주입 로직

--- src/main/java/global/ioc/IocContainerConfig.java
+++ src/main/java/global/ioc/IocContainerConfig.java
@@ -13,14 +13,14 @@ import org.slf4j.Logger;
 public class IocContainerConfig {
 
     private Logger log;
-    private final Set<Class<? extends AbstractBean>> beanClasses =
+    private Set<Class<? extends AbstractBean>> beanClasses =
             new Reflections(new ConfigurationBuilder().forPackages("global", "domain"))
                     .getSubTypesOf(AbstractBean.class); // 패키지 스캔 후, 추상 빈 상속구현체 전부 갖고오기
-    private final Map<Class<?>, AbstractBean> singletonBeans = new HashMap<>(); // 싱글톤 빈 캐시
+    private Map<Class<?>, AbstractBean> singletonBeans = new HashMap<>(); // 싱글톤 빈 캐시
 
     // 의존성 주입 위상 정렬을 위한 그래프 필드 + 진입차수
-    private final Map<Class<?>, Set<Class<?>>> dependencyGraph = new HashMap<>();
-    private final Map<Class<?>, Integer> indegree = new HashMap<>();
+    private Map<Class<?>, Set<Class<?>>> dependencyGraph = new HashMap<>();
+    private Map<Class<?>, Integer> indegree = new HashMap<>();
 
     /**
      * IoC 컨테이너 동작 메소드
@@ -31,54 +31,76 @@ public class IocContainerConfig {
         log.info("[{}] : IoC 컨테이너 동작 시작", this.getClass().getSimpleName());
 
         // 1. 빈 생성 & 빈 간 의존성 관리(IoC 띄운 다음에 수행하므로 리플랙션 기반)
-        initBasicConstructorBeans(); // 기본 생성자 보유 AbstractBean 구현체들 먼저 newInstance() 호출
+        initAutowiredConstructorBeans(); // 위상정렬 기반 빈 생성 및 의존성 주입
 
-        // 2. 빈에 로거 주입
-
-        // 3. 빈 초기화(AbstractBean의 init 메소드 호출
-
-        // 4. 빈 캐싱(싱글톤 관리)
-
-        // 5. 서버 종료시, 빈 destroy
-    }
-
-    /**
-     * 기본 생성자만 있는 빈 생성
-     */
-    private void initBasicConstructorBeans() {
-        // 기본 생성자만 있는 빈들 생성하기
-        for (Class<? extends AbstractBean> beanClass: beanClasses) {
-            try {
-                // 생성자가 1개이며, 생성자 파라미터가 없으면 기본 생성자
-                if (beanClass.getDeclaredConstructors().length == 1
-                        && beanClass.getDeclaredConstructors()[0].getParameterCount() == 0) {
-                    Constructor<? extends AbstractBean> constructor = beanClass.getConstructor();
-                    constructor.setAccessible(true); // private 회피
-                    AbstractBean bean = constructor.newInstance();
-
-                    bean.init(); // 빈 초기화
-                    singletonBeans.put(beanClass, bean); // 싱글톤 캐싱
-                }
-            } catch (NoSuchMethodException
-                     | IllegalAccessException
-                     | InstantiationException
-                     | InvocationTargetException e) {
-                log.error("{} 생성자 생성 리플렉션 예외 발생: {}", e.getClass().getSimpleName(), e.getMessage());
-                throw new RuntimeException(e);
-            }
+        // 2. 빈 초기화(AbstractBean의 init 메소드 호출)
+        for (Map.Entry<Class<?>, AbstractBean> entry: singletonBeans.entrySet()) {
+            entry.getValue().init();
         }
+
+        // 3. 서버 종료시, 빈 destroy
+        for (Map.Entry<Class<?>, AbstractBean> entry: singletonBeans.entrySet()) {
+            entry.getValue().destroy();
+        }
+
+        // 4. 서버 종료시, 빈 관련 필드들 전부 정리
+        beanClasses = null;
+        singletonBeans = null;
+        dependencyGraph = null;
+        indegree = null;
     }
 
     /**
-     * 자동 생성자 주입 빈 생성
+     * <p>1. 이미 초기화 완료한 기본 생성자 기반 빈이 생성자 주입의 재료가 된다</p>
+     * <p>2. 우선 순위는 기본 생성자 빈의 의존성 주입 빈 -> 의존성 주입 빈의 또다른 의존성 주입 빈으로 이뤄져야만 한다</p>
+     * <p>3. 이를 위해 IoC 컨테이너에서는 어떤 알고리즘을 쓸까? : 위상정렬 생각했는데 진짜 IoC도 위상정렬 쓰네 ㅇㅂㅇ</p>
+     * <p>좀더 정확히는 유향 비순환 그래프를 기반으로 의존성 구조를 형성(https://umbum.dev/876/?utm_source=chatgpt.com)</p>
+     * <p>4. 의존성 주입이 여러 개라면? -> 일단 가장 파라미터 많은 생성자로만 하자...</p>
+     * <p>5. 객체 회수할 때는 어떻게 할까? -> 싱글톤 캐시를 비워버리고 GC는 자동으로 객체 할당 해제</p>
      */
     private void initAutowiredConstructorBeans() {
-        // 1. 이미 초기화 완료한 기본 생성자 기반 빈이 생성자 주입의 재료가 된다
-        // 2. 우선 순위는 기본 생성자 빈의 의존성 주입 빈 -> 의존성 주입 빈의 또다른 의존성 주입 빈으로 이뤄져야만 한다
-        // 3. 이를 위해 IoC 컨테이너에서는 어떤 알고리즘을 쓸까? : 위상정렬 생각했는데 진짜 IoC도 위상정렬 쓰네 ㅇㅂㅇ
-        // 좀더 정확히는 유향 비순환 그래프를 기반으로 의존성 구조를 형성(https://umbum.dev/876/?utm_source=chatgpt.com)
-        // 4. 의존성 주입이 여러 개라면?
-        // 5. 객체 회수할 때는 어떻게 할까? -> 싱글톤 캐시를 비워버리고 GC는 자동으로 객체 할당 해제
+        // 진입차수 0인 애들(기본 생성자 빈)부터 큐 산입
+        Queue<Class<? extends AbstractBean>> queue = new LinkedList<>();
+        for (Map.Entry<Class<?>, Integer> entry : indegree.entrySet()) {
+            if (entry.getValue() == 0) {
+                queue.add((Class<? extends AbstractBean>) entry.getKey());
+            }
+        }
+
+        // 위상정렬 시작
+        while(!queue.isEmpty()) {
+            Class<? extends AbstractBean> beanClass = queue.poll();
+
+            try {
+                // 생성자 선택 (파라미터가 가장 많은 생성자)
+                Constructor<?> constructor = Arrays.stream(beanClass.getDeclaredConstructors())
+                        .max(Comparator.comparingInt(Constructor::getParameterCount))
+                        .orElseThrow(() -> new RuntimeException(beanClass.getSimpleName() + " 빈 생성자 확인 안됨"));
+
+                // 생성자 파라미터를 싱글톤 캐시에서 가져와 준비
+                Class<?>[] paramTypes = constructor.getParameterTypes();
+                Object[] params = Arrays.stream(paramTypes)
+                        .map(singletonBeans::get) // 이미 순차적으로 싱글톤 캐싱 처리됐을 거야
+                        .toArray();
+
+                constructor.setAccessible(true);
+                AbstractBean bean = (AbstractBean) constructor.newInstance(params);
+
+                // 싱글톤 캐싱
+                singletonBeans.put(beanClass, bean);
+            } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+                log.error("빈 생성 실패: {}", e.getMessage());
+                throw new RuntimeException(e);
+            }
+
+            // 의존성 그래프에서 연결된 후속 빈 진입차수 감소
+            for (Class<?> adj : dependencyGraph.get(beanClass)) {
+                indegree.put(adj, indegree.get(adj) - 1);
+                if (indegree.get(adj) == 0) { // 진입차수 0이면 큐 산입
+                    queue.add((Class<? extends AbstractBean>) adj);
+                }
+            }
+        }
     }
 
     /**
@@ -92,7 +114,8 @@ public class IocContainerConfig {
 
         for (Class<? extends AbstractBean> beanClass : beanClasses) {
             // 기본 생성자는 제외
-            if (singletonBeans.containsKey(beanClass)) {
+            if (beanClass.getDeclaredConstructors().length == 1
+                    && beanClass.getDeclaredConstructors()[0].getParameterCount() == 0) {
                 indegree.put(beanClass, 0); // 진입차수 0
                 continue;
             }
@@ -112,12 +135,5 @@ public class IocContainerConfig {
             }
             indegree.put(beanClass, paramCount); // 진입차수 카운팅
         }
-    }
-
-    /**
-     * 싱글톤 캐시에서 빈 조회하기
-     */
-    private <T extends AbstractBean> T getBean(Class<T> beanClass) {
-        return beanClass.cast(singletonBeans.get(beanClass));
     }
 }
