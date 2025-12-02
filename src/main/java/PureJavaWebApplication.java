f22ae1b06e4def809a193d93fafba6265f81d0a2
kimD0ngjun <chickentasty0112@gmail.com>
2025-12-02 22:58:59 +0900

    additional setting

--- src/main/java/PureJavaWebApplication.java
+++ src/main/java/PureJavaWebApplication.java
@@ -1,5 +1,17 @@
-public class PureJavaWebApplication {
-    public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class PureJavaWebApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(PureJavaWebApplication.class);
+
+    public static void main(String[] args) {
+        log.info("""
+                
+                =====================================
+                PURE JAVA WEB APPLICATION RUNNING...!
+                =====================================
+                
+                """);
     }
 }
