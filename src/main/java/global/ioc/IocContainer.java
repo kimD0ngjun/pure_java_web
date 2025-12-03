51cec8c8c7d13cf04b7bb5e513ee2fa7add97820
kimD0ngjun <chickentasty0112@gmail.com>
2025-12-03 22:07:37 +0900

    fix: 빈 AOP 적용 정책 필요

--- src/main/java/global/ioc/IocContainer.java
+++ src/main/java/global/ioc/IocContainer.java
@@ -25,9 +25,14 @@ public class IocContainer {
         // 빈 스캔
         config.scanBeans();
 
-        // 빈 생성 & 빈 간 의존성 관리 + 초기화(IoC 띄운 다음에 수행하므로 리플랙션 기반)
+        // 빈 생성 & 빈 간 의존성 관리(IoC 띄운 다음에 수행하므로 리플랙션 기반)
         config.setDependencyGraph();
         config.constructAutowiredBeans(); // 위상정렬 기반 빈 생성 및 의존성 주입
+
+        // 빈 생성과 의존성 주입이 이뤄진 후 초기화 전에 AOP가 적용된다
+        // 즉 빈들의 로깅 정책과 트랜잭션 정책 적용을 프록시 패턴 활용해서 여기서 적용해야할듯
+
+        // 빈 초기화
         config.initBeans();
     }
 
